ef86126ab043cfad55d99339f30086854ff232a9
Mamoud Elawadi <mahmoudwagdi86@gmail.com>
2024-10-16 21:32:41 +0200

    chore: use updated apollo wrapper

--- src/lib/apollo-wrapper.tsx
+++ src/lib/apollo-wrapper.tsx
@@ -1,35 +1,37 @@
-"use client";
+'use client';
+// ^ this file needs the "use client" pragma
 
+import { HttpLink } from '@apollo/client';
 import {
-    ApolloLink,
-    HttpLink,
-} from "@apollo/client";
-import {
-    ApolloNextAppProvider, NextSSRApolloClient,
-    NextSSRInMemoryCache,
-    SSRMultipartLink,
-} from "@apollo/experimental-nextjs-app-support/ssr";
+    ApolloNextAppProvider,
+    ApolloClient,
+    InMemoryCache,
+} from '@apollo/experimental-nextjs-app-support';
 
+// have a function to create a client for you
 function makeClient() {
+    console.log('makeClient', process.env.API_URL);
     const httpLink = new HttpLink({
-        // https://studio.apollographql.com/public/spacex-l4uc6p/
-        uri: "https://main--spacex-l4uc6p.apollographos.net/graphql",
+        // this needs to be an absolute url, as relative urls cannot be used in SSR
+        uri: 'https://footy-directory-59316565a7b1.herokuapp.com/',
+        // you can disable result caching here if you want to
+        // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
+        fetchOptions: { cache: 'no-store' },
+        // you can override the default `fetchOptions` on a per query basis
+        // via the `context` property on the options passed as a second argument
+        // to an Apollo Client data fetching hook, e.g.:
+        // const { data } = useSuspenseQuery(MY_QUERY, { context: { fetchOptions: { cache: "force-cache" }}});
     });
 
-    return new NextSSRApolloClient({
-        cache: new NextSSRInMemoryCache(),
-        link:
-            typeof window === "undefined"
-                ? ApolloLink.from([
-                    new SSRMultipartLink({
-                        stripDefer: true,
-                    }),
-                    httpLink,
-                ])
-                : httpLink,
+    // use the `ApolloClient` from "@apollo/experimental-nextjs-app-support"
+    return new ApolloClient({
+        // use the `InMemoryCache` from "@apollo/experimental-nextjs-app-support"
+        cache: new InMemoryCache(),
+        link: httpLink,
     });
 }
 
+// you need to create a component to wrap your app in
 export function ApolloWrapper({ children }: React.PropsWithChildren) {
     return (
         <ApolloNextAppProvider makeClient={makeClient}>
