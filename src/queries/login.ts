daa6b6642fda362862c1dd8666c043051e3f768d
Mamoud Elawadi <mahmoudwagdi86@gmail.com>
2024-10-26 12:10:57 +0200

    chore: extract login queries

---
+++ src/queries/login.ts
@@ -0,0 +1,28 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_QUERY = gql`
+    query Query {
+        login {
+            token
+            user {
+                id
+                email
+                first_name
+                last_name
+            }
+        }
+    }
+`;
+export const LOGIN_MUTATION = gql`
+    mutation LoginMutation($input: LoginInput!) {
+        login(input: $input) {
+            token
+            user {
+                id
+                email
+                first_name
+                last_name
+            }
+        }
+    }
+`;
