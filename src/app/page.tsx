555ef76d2c39405dd1e13f319bbb1913cfb562e3
Mamoud Elawadi <mahmoudwagdi86@gmail.com>
2024-11-03 13:58:48 +0100

    chore: use types in search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import Search from 'components/Search';
+import SearchSection from 'components/Search';
 
-const Home = async () => {
+const Home = () => {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Search />
+        <SearchSection />
       </main>
     </div>
   );
--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,8 +2,9 @@ import { ChangeEvent, useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { SEARCH_QUERY } from 'queries/search';
 import { Input } from 'components/ui/input';
+import { Club, Nation, Player } from 'src/generated/graphql';
 
-const Search = () => {
+const SearchSection = () => {
     const [search, setSearch] = useState('');
     const { data } = useQuery(SEARCH_QUERY, {
         variables: { input: search },
@@ -15,15 +16,14 @@ const Search = () => {
     };
 
     return (
-        <div>
-            <h1>Search</h1>
+        <>
             <Input onChange={handleChange} name="search" type="search" value={search} />
             <div className="flex flex-col gap-5 mt-2">
                 <h2 className="font-extrabold text-xl">Results</h2>
                 {data?.search.nations.length > 0 && (
                     <div>
                         <h3 className="font-bold text-lg">Nations</h3>
-                        {data.search.nations.map(nation => (
+                        {data.search.nations.map((nation: Nation) => (
                             <p key={`nation_${nation.id}`}>{nation.name}</p>
                         ))}
                     </div>
@@ -31,7 +31,7 @@ const Search = () => {
                 {data?.search.clubs.length > 0 && (
                     <div>
                         <h3 className="font-bold text-lg">Clubs</h3>
-                        {data.search.clubs.map(club => (
+                        {data.search.clubs.map((club: Club) => (
                             <p key={`club_${club.id}`}>{club.name}</p>
                         ))}
                     </div>
@@ -39,14 +39,14 @@ const Search = () => {
                 {data?.search.players.length > 0 && (
                     <div>
                         <h3 className="font-bold text-lg">Players</h3>
-                        {data.search.players.map(player => (
+                        {data.search.players.map((player: Player) => (
                             <p key={`player_${player.id}`}>{player.name}</p>
                         ))}
                     </div>
                 )}
             </div>
-        </div>
+        </>
     );
 };
 
-export default Search;
+export default SearchSection;
