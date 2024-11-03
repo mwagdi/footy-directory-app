32b7b447ad11cec9ccee37dd5fb68148d0f0a57e
Mamoud Elawadi <mahmoudwagdi86@gmail.com>
2024-11-03 12:36:33 +0100

    feature: render search results

--- src/app/dashboard/search/page.tsx
+++ src/app/dashboard/search/page.tsx
@@ -24,6 +24,33 @@ const Search = () => {
         <div>
             <h1>Search</h1>
             <Input onChange={handleChange} name="search" type="search" value={search} />
+            <div className="flex flex-col gap-5 mt-2">
+                <h2 className="font-extrabold text-xl">Results</h2>
+                {data?.search.nations.length > 0 && (
+                    <div>
+                        <h3 className="font-bold text-lg">Nations</h3>
+                        {data.search.nations.map(nation => (
+                            <p key={`nation_${nation.id}`}>{nation.name}</p>
+                        ))}
+                    </div>
+                )}
+                {data?.search.clubs.length > 0 && (
+                    <div>
+                        <h3 className="font-bold text-lg">Clubs</h3>
+                        {data.search.clubs.map(club => (
+                            <p key={`club_${club.id}`}>{club.name}</p>
+                        ))}
+                    </div>
+                )}
+                {data?.search.players.length > 0 && (
+                    <div>
+                        <h3 className="font-bold text-lg">Players</h3>
+                        {data.search.players.map(player => (
+                            <p key={`player_${player.id}`}>{player.name}</p>
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
