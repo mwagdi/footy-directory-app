376a47885dde53550ac9367ffc1be6f0f0e6d415
Mamoud Elawadi <mahmoudwagdi86@gmail.com>
2024-11-29 18:49:15 +0100

    chore: enable upload in apollo client

--- src/lib/apollo-wrapper.tsx
+++ src/lib/apollo-wrapper.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { HttpLink } from '@apollo/client';
 import {
     ApolloNextAppProvider,
     ApolloClient,
     InMemoryCache,
 } from '@apollo/experimental-nextjs-app-support';
 import { persistCache } from 'apollo3-cache-persist';
+import createUploadLink from 'apollo-upload-client/createUploadLink.mjs';
+import { PropsWithChildren } from 'react';
 
 const cache = new InMemoryCache();
 
@@ -21,16 +22,13 @@ const cache = new InMemoryCache();
 
 // have a function to create a client for you
 const makeClient = () => {
-    const httpLink = new HttpLink({
-        // this needs to be an absolute url, as relative urls cannot be used in SSR
+    const uploadLink = createUploadLink({
         uri: 'https://footy-directory-59316565a7b1.herokuapp.com/',
-        // you can disable result caching here if you want to
-        // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
         fetchOptions: { cache: 'no-store' },
-        // you can override the default `fetchOptions` on a per query basis
-        // via the `context` property on the options passed as a second argument
-        // to an Apollo Client data fetching hook, e.g.:
-        // const { data } = useSuspenseQuery(MY_QUERY, { context: { fetchOptions: { cache: "force-cache" }}});
+        headers: {
+           'Apollo-Require-Preflight': 'true',
+            // 'Content-Type': 'application/json',
+        }
     });
 
 
@@ -39,12 +37,12 @@ const makeClient = () => {
     return new ApolloClient({
         // use the `InMemoryCache` from "@apollo/experimental-nextjs-app-support"
         cache,
-        link: httpLink,
+        link: uploadLink,
     });
 };
 
 // you need to create a component to wrap your app in
-export function ApolloWrapper({ children }: React.PropsWithChildren) {
+export function ApolloWrapper({ children }: PropsWithChildren) {
     return (
         <ApolloNextAppProvider makeClient={makeClient}>
             {children}
